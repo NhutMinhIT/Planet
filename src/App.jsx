2359a306a68835e074bcfc6ce982b6524e3e5463
FPTnhutminh00 <nhutminh.fpt@gmail.com>
2022-08-10 22:46:35 +0700

    upload file About and Feedback

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Router } from 'react-router-dom';
 import Navbar from './Components/NavbarComponent/Navbar';
 import Banner from './Components/BannerComponent/Banner';
 import HomePage from './Components/HomeComponent/Home';
 import ContactPage from './Components/ContactComponent/Contact';
 import './App.css';
+import AboutPage from './Components/AboutComponent/About';
+import FeedbackPage from './Components/FeedbackComponents/Feedback'
 
 function App() {
   return (
     <div className="App">
+
       <Navbar />
       <Banner />
       <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/contact" element={<ContactPage />} />
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/home" element={<HomePage />} />
         <Route path="/tree" element={<ContactPage />} />
         <Route path="/benefits" element={<ContactPage />} />
-      </Routes>
-    </div>
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/feedback" element={<FeedbackPage />} />
+
+      </Routes >
+    </div >
 
   );
 }
---
+++ src/Components/AboutComponent/About.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './About.css';
+
+
+const About = () => {
+    return (
+        <div className='nature'>
+            <div className="abouts">
+                <span>Nature</span>
+                <div className="about">
+
+                    <div className="cricle">34+</div>
+                    <span>Nation Parks</span>
+                    <span>In VietNam</span>
+                </div>
+                <div className="about">
+                    <div className="cricle">147+</div>
+                    <span>Wild Animals</span>
+                    <span>In VietNam</span>
+                </div>
+                <div className="about">
+                    <div className="cricle">20.000+</div>
+                    <span>Plant</span>
+                    <span>In VietNam</span>
+                </div>
+
+            </div >
+            <div className="abouts">
+                <span>Calamity 2021</span>
+                <div className="about">
+
+                    <div className="cricle">841+</div>
+                    <span>Natural Disaster</span>
+                    <span>In VietNam</span>
+                </div>
+                <div className="about">
+                    <div className="cricle">14/22</div>
+                    <span>Type of Disaster</span>
+                    <span>In VietNam</span>
+                </div>
+                <div className="about">
+                    <div className="cricle">12+</div>
+                    <span>Storms</span>
+                    <span>In VietNam</span>
+                </div>
+
+            </div >
+
+        </div>
+
+
+    )
+}
+
+export default About;
