3df7525c15d7995f53d7018066008199208af8dc
FPTnhutminh00 <nhutminh.fpt@gmail.com>
2022-08-24 11:32:49 +0700

    Uplaod TreeComponent

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/NavbarComponent/Navbar';
 import Banner from './Components/BannerComponent/Banner';
 import HomePage from './Components/HomeComponent/Home';
-import './App.css';
+import TreePage from './Components/TreeComponent/Tree'
 import AboutPage from './Components/AboutComponent/About';
 import FeedbackPage from './Components/FeedbackComponents/Feedback';
 import TravelPage from './Components/TravelComponent/Travel';
 import Footer from './Components/FooterComponent/Footer';
+import './App.css';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/home" element={<HomePage />} />
+        <Route path='/tree' element={<TreePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/travel" element={<TravelPage />} />
         <Route path="/feedback" element={<FeedbackPage />} />
